fad5d496add499bb897e80e4eda30d449adedb55
Samuel <samuel.jaglale-dalleau@epitech.eu>
2025-10-07 19:54:46 +0400

    add a navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import './Header.css'
 
 function Header() {
-  return (
-    <header>
-      <h1>Samuel - JAGLALE DALLEAU</h1>
-        <p>Développeur web full-stack / Créateur de sites</p>
+ return (
+    <header id="acceuil">
+      <nav className="navbar">
+        <h1 className="logo">Samuel JD</h1>
+        <ul className="nav-links">
+          <li><a href="#acceuil">Acceuil</a></li>
+          <li><a href="#apropos">À propos</a></li>
+          <li><a href="#competences">Compétences</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      <div className="header-content">
+        <h2>Samuel - JAGLALE DALLEAU</h2>
+        <p>Développeur web full-stack & créateur d’expériences digitales</p>
+      </div>
     </header>
   )
 }
